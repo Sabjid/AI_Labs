c0ec29810e6aac8ff5a64985f916c55a7946094c
Saba Sajid <sabasajid2620@gmail.com>
2024-11-05 11:56:28 +0500

    Add files via upload

---
+++ Lab5.py
@@ -0,0 +1,27 @@
+                         # Task : 1
+
+for i in range (1 , 11):
+      print(i)  
+
+                          # Task : 2   
+
+sum = 0
+for i in range(1 , 11):
+    sum += i
+print("The Sum of all Numbers is :",sum)
+
+                        # Task : 3
+                         
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+                # Task : 4
+
+for i in range (1 , 21):
+    if i %  2 == 0 :
+         print ( "Even Numbers is :", i)
+
+                                                  
